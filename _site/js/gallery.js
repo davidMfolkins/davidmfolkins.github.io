a0b1deb404c38ed68c5c85ad971088f1515c4546
David Folkins <david.folkins88@gmail.com>
2020-04-29 14:26:35 -0400

    Added picture selection functionality

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,12 @@
 function activateGallery () {
-  alert("Hello from the gallery file!");
+  let thumbNails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo > img");
+
+  thumbNails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function(){
+// Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    });
+  });                      
 }
