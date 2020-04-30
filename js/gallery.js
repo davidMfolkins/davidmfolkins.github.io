c48201c1a221ee8540774825ac7e467d552fb15c
David Folkins <david.folkins88@gmail.com>
2020-04-30 10:31:28 -0400

    Finish the Javascript on Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,28 @@
+// Activates the image gallery
+
+
 function activateGallery () {
   let thumbNails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage  = document.querySelector("#gallery-photo > img");
 
   thumbNails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function(){
-// Set clicked image as main image.
+// Set clicked image as main image
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+// Change which image is current
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+// Update image info
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });                      
 }
